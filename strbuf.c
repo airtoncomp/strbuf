456f7011dcb7ab7eaea8dcfcdeb5bf6df6130039
airtoncomp <airtonishimori@gmail.com>
2026-09-03 21:52:51 -0300

    Improve append function. Call alloc_buf() when sb->cap == 0 or realloc_buf() otherwise.

--- strbuf.c
+++ strbuf.c
@@ -66,7 +66,8 @@
             }                                                       \
         } while(0)                                          
 
-#define BUFLEN_GROWTH_FACTOR   2
+#define BUFLEN_GROWTH_FACTOR    2
+#define MAX(a, b)               ((a) > (b) ? (a) : (b))
 
 static int realloc_buf(strbuf_t *sb, size_t new_cap)
 {
@@ -82,6 +83,7 @@ static int alloc_buf_zero(strbuf_t *sb, size_t cap)
     sb->data = calloc(cap, sizeof(*sb->data));
     SB_RET_ERR_ON_NULL(sb->data, "failed to alloc memory");
     sb->cap = cap;
+    sb->len = 0;
     return 0;
 }
 
@@ -90,22 +92,22 @@ static int alloc_buf(strbuf_t *sb, size_t cap)
     sb->data = malloc(cap * sizeof(*sb->data));
     SB_RET_ERR_ON_NULL(sb->data, "failed to alloc memory");
     sb->cap = cap;
+    sb->len = 0;
     return 0;
 }
 
 int sb_init(strbuf_t *sb, size_t cap)
 {
     SB_RET_ERR_ON_NULL(sb, "strbuf_t* cannot be null");
-    sb->len = 0;
     return alloc_buf_zero(sb, cap); 
 }
 
 int sb_init_str(strbuf_t *sb, const char *str, size_t slen)
 {
     SB_RET_ERR_ON_NULL(sb, "strbuf_t* cannot be null");
-    sb->len = slen;
     if (alloc_buf(sb, slen) < 0)
         return -1;
+    sb->len = slen;
     strcpy(sb->data, str);
     return 0;
 }
@@ -115,12 +117,15 @@ int sb_from_cstr(strbuf_t *sb, const char *cstr)
     SB_RET_ERR_ON_NULL(sb, "strbuf_t* cannot be null");
     SB_RET_ERR_ON_NULL(sb->data, "string buffer *data is null");
     size_t len = strlen(cstr);
-    if (sb->cap <= len) {
-        if (realloc_buf(sb, sb->cap * BUFLEN_GROWTH_FACTOR + len) < 0)
+    if (sb->cap == 0) {
+        if (alloc_buf(sb, len) < 0)
+            return -1;
+    } else if (sb->cap > 0 && sb->cap <= len) {
+        if (realloc_buf(sb, sb->cap * BUFLEN_GROWTH_FACTOR) < 0)
             return -1;
     }
     strcpy(sb->data, cstr);
-    sb->len = strlen(cstr);
+    sb->len = len;
     return 0;
 }
 
@@ -152,7 +157,7 @@ int sb_reserve(strbuf_t *sb, size_t required)
 {
     SB_RET_ERR_ON_NULL(sb, "strbuf_t* cannot be null");
     if (sb->cap <= required)
-        return realloc_buf(sb, required);
+        return realloc_buf(sb, required + 1);
     return 0;
 }
 
@@ -161,22 +166,30 @@ int sb_append_cstr(strbuf_t *sb, char *cstr)
     SB_RET_ERR_ON_NULL(sb, "strbuf_t* cannot be null");
     SB_RET_ERR_ON_NULL(cstr, "c string pointer cannot be null");
     size_t len = strlen(cstr);
-    size_t tot_len = sb->len + len;
-    if (sb->cap <= tot_len) {
-        if (realloc_buf(sb, sb->cap * BUFLEN_GROWTH_FACTOR + tot_len) < 0)
+    size_t tot_len = sb->len + len + 1;
+    size_t new_cap = MAX(sb->cap * BUFLEN_GROWTH_FACTOR, tot_len);
+    if (sb->cap == 0) {
+        if (alloc_buf(sb, new_cap) < 0)
+            return -1;
+    } else {
+        if (realloc_buf(sb, sb->cap * BUFLEN_GROWTH_FACTOR) < 0)
             return -1;
     }
     strcat(sb->data, cstr);
-    sb->len = tot_len;
+    sb->len += len;
     return 0;
 }
 
 int sb_append_char(strbuf_t *sb, char c)
 {
     SB_RET_ERR_ON_NULL(sb, "strbuf_t* cannot be null");
-    size_t tot_len = sb->len + 1;
-    if (sb->cap <= tot_len) {
-        if (realloc_buf(sb, sb->cap * BUFLEN_GROWTH_FACTOR + tot_len))
+    size_t tot_len = sb->len + 1 + 1;
+    size_t new_cap = MAX(sb->cap * BUFLEN_GROWTH_FACTOR, tot_len);
+    if (sb->cap == 0) {
+        if (alloc_buf(sb, new_cap) < 0)
+            return -1;
+    } else {
+        if (realloc_buf(sb, sb->cap * BUFLEN_GROWTH_FACTOR) < 0)
             return -1;
     }
     sb->data[sb->len++] = c;
